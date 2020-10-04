0eab757adeb2dbe3fa28c331f84f7e89b830bf68
Olena Onopriyenko <54893280+OleOno974@users.noreply.github.com>
2020-10-03 20:09:03 -0400

    Add files via upload

---
+++ Workshops/Workshop2-3/src/shapes/Shape.java
@@ -0,0 +1,22 @@
+// Name: Olena Onopriyenko
+// Seneca Student ID:124605163 
+// Seneca email: oonopriyenkko@myseneca.ca
+// Date of completion: 2020-01-30
+//
+// I confirm that the content of this file is created by me,
+//   with the exception of the parts provided to me by my professor.
+//
+// Workshop 2 & 3
+// Shape.java
+
+package shapes;
+
+/** This class creates Shape interface */
+public interface Shape {
+	
+	/** 
+	 * Creates an abstract method to calculate shape's perimeter
+	 * @return a double indicating the perimiter of the shape
+	 */
+	double calculatePerimeter();
+}
