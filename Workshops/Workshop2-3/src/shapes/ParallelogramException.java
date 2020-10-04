51b7304b306bbdd63f27ee90602a6ae6d4ba21fc
Olena Onopriyenko <54893280+OleOno974@users.noreply.github.com>
2020-10-03 20:07:10 -0400

    Add files via upload

---
+++ Workshops/Workshop2-3/src/shapes/ParallelogramException.java
@@ -0,0 +1,13 @@
+package shapes;
+
+/** This class handles exceptions for the Parallelogram class */
+public class ParallelogramException extends Exception {
+
+	/**
+	 * Constructor accepts a string for the message	
+	 * @param message A string representing the message
+	 */
+	public ParallelogramException(String message) {
+		super(message);
+	}
+}
